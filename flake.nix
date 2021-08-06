adca02ef27f45a7bd3652c4ef808a68ee5863420
benton <bentonedmondson@gmail.com>
2021-08-06 13:51:30 -0500

    cleaned up flake file

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
 {
   inputs = {
-    
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     libgourou-utils.url = "github:BentonEdmondson/libgourou-utils";
@@ -16,33 +15,35 @@
     inept-epub = flakes.inept-epub.defaultPackage.x86_64-linux;
   in {
     defaultPackage.x86_64-linux = nixpkgs.stdenv.mkDerivation {
-        pname = "knock";
-        version = "0.1.0-alpha";
-        src = self;
-        nativeBuildInputs = [ nixpkgs.makeWrapper ];
-        buildInputs = [
-          (nixpkgs.python3.withPackages(python3Packages: [ python3Packages.python_magic ]))
-          libgourou-utils inept-epub
-        ];
-        installPhase = ''
-          mkdir -p $out/bin
-          chmod +x knock
-          cp knock $out/bin
-          wrapProgram $out/bin/knock --prefix PATH : ${nixpkgs.lib.makeBinPath [libgourou-utils inept-epub]}
-        '';
+      pname = "knock";
+      version = "0.1.0-alpha";
+      src = self;
 
-        meta = {
-          description = "A CLI tool to convert ACSM files to DRM-free EPUB files";
-          homepage = "https://github.com/BentonEdmondson/knock";
-          license = [ nixpkgs.lib.licenses.gpl3Only ];
-          maintainers = [{
-            name = "Benton Edmondson";
-            email = "bentonedmondson@gmail.com";
-          }];
-          # potentially others, but I'm only listed those tested
-          platforms = [ "x86_64-linux" ];
-        };
+      nativeBuildInputs = [ nixpkgs.makeWrapper ];
+
+      buildInputs = [
+        (nixpkgs.python3.withPackages(python3Packages: [ python3Packages.python_magic ]))
+        libgourou-utils inept-epub
+      ];
+
+      installPhase = ''
+        mkdir -p $out/bin
+        chmod +x knock
+        cp knock $out/bin
+        wrapProgram $out/bin/knock --prefix PATH : ${nixpkgs.lib.makeBinPath [libgourou-utils inept-epub]}
+      '';
+
+      meta = {
+        description = "A CLI tool to convert ACSM files to DRM-free EPUB files";
+        homepage = "https://github.com/BentonEdmondson/knock";
+        license = [ nixpkgs.lib.licenses.gpl3Only ];
+        maintainers = [{
+          name = "Benton Edmondson";
+          email = "bentonedmondson@gmail.com";
+        }];
+        # potentially others, but I'm only listed those tested
+        platforms = [ "x86_64-linux" ];
       };
-
+    };
   };
 }
